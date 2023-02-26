47e6519775e3e7e3c27ce6137d4ab08eac07d5bf
Jorge Caballeros Pérez <cab20009@uvg.edu.gt>
2023-02-26 02:04:02 -0600

    Finish

--- AFN.py
+++ AFN.py
@@ -4,8 +4,8 @@ Jorge Caballeros Pérez
 Laboratorio A
 Implementación de programa el cual recibe una ER y como resultado genera AFN con sus estados de aceptación y transiciones correspondientes.
 """
-import re
-from graphviz import Digraph
+
+import graphviz
 from collections import deque
 
 
@@ -62,63 +62,76 @@ class Regex:
     def listPostfix(self):
         return [i for i in self.postfix]
 
+class AFN:
+    def __init__(self, alphabet, postfix):
+        self.alphabet = alphabet
+        self.postfix = postfix
+        self.states = set()
+        self.transitions = []
+        self.start_state = None
+        self.final_states = set()
+        self.generate_afn()
 
-class State:
-    """Representa un estado en un AFN."""
+    def generate_afn(self):
+        stack = []
+        for char in self.postfix:
+            if char in self.alphabet:
+                state1 = 'q' + str(len(self.states))
+                state2 = 'q' + str(len(self.states) + 1)
+                self.states.add(state1)
+                self.states.add(state2)
+                self.transitions.append((state1, char, state2))
+                stack.append((state1, state2))
+            elif char == '*':
+                state1, state2 = stack.pop()
+                self.transitions.append((state2, '', state1))
+                self.transitions.append((state1, '', state2))
+                self.states.add(state1)
+                self.states.add(state2)
+                stack.append((state1, state2))
+            elif char == '%':
+                state2, state2 = stack.pop()
+                state1, state1 = stack.pop()
+                self.transitions.append((state1, '', state2))
+                self.states.add(state1)
+                self.states.add(state2)
+                stack.append((state1, state2))
+            elif char == '|':
+                state2, state2 = stack.pop()
+                state1, state1 = stack.pop()
+                new_start_state = 'q' + str(len(self.states))
+                new_final_state = 'q' + str(len(self.states) + 1)
+                self.states.add(new_start_state)
+                self.states.add(new_final_state)
+                self.transitions.append((new_start_state, '', state1))
+                self.transitions.append((new_start_state, '', state2))
+                self.transitions.append((state1, '', new_final_state))
+                self.transitions.append((state2, '', new_final_state))
+                stack.append((new_start_state, new_final_state))
+        self.start_state, self.final_states = stack.pop()
 
-    def __init__(self, label=None, edges=None):
-        self.label = label
-        self.edges = edges or []
+    def to_graphviz(self):
+        graph = graphviz.Digraph()
+        graph.node(self.start_state, shape='point')
+        for state in self.states:
+            graph.node(state, shape='circle')
+            if state in self.final_states:
+                graph.attr('node', shape='doublecircle')
+            else:
+                graph.attr('node', shape='circle')
+            for transition in self.transitions:
+                if transition[0] == state:
+                    label = transition[1] if transition[1] != '' else 'ε'
+                    graph.edge(transition[0], transition[2], label=label)
+        return graph
 
 
-class NFA:
-    def __init__(self, postfix) -> None:
-        self.postFix = postfix
-
-
-def postfix_to_nfa(postfix):
-    """Convierte una expresión regular en formato postfix a un AFN."""
-    stack = []
-    for char in postfix:
-        if char == "%":
-            second, first = stack.pop(), stack.pop()
-            first.concatenate(second)
-            stack.append(first)
-        elif char == "|":
-            second, first = stack.pop(), stack.pop()
-            first.alternate(second)
-            stack.append(first)
-        elif char == "*":
-            nfa = stack.pop()
-            nfa.kleene_star()
-            stack.append(nfa)
-        else:
-            state = State(label=char)
-            nfa = NFA(start=state, end=state)
-            stack.append(nfa)
-    return stack.pop()
-
-
-def visualize_nfa(nfa):
-    """Visualiza un AFN utilizando Graphviz."""
-    graph = Digraph()
-    nodes, edges = set(), set()
-    start, end = nfa.start, nfa.end
-    nodes.update([start, end])
-    for current in nodes:
-        if current == end:
-            graph.node(str(current.label), shape='doublecircle')
-        else:
-            graph.node(str(current.label), shape='circle')
-        for edge in current.edges:
-            nodes.add(edge)
-            edges.add((current, edge))
-    for start, end in edges:
-        graph.edge(str(start.label), str(end.label), label='ε')
-    return graph
-
-
-regex = Regex('ab|*cd.|')
+alfabeto = set('abcd')
+regex = Regex('a+b')
 print(f"Infix: {regex.infix}\nPostfix: {regex.postfix}")
-nfa = NFA.postfix_to_nfa(regex.postfix)
-nfa.render('nfa')
+
+afn = AFN(alfabeto, regex.postfix)
+
+graph = afn.to_graphviz()
+
+graph.view()
